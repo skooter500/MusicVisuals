c750dfc61c2df7f10f02ae441fdd6b5fd0dde632
Bryan Duggan <skooter500@gmail.com>
2021-03-22 11:54:02 +0000

    bla

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -10,7 +10,7 @@ public class Main
 	public void startUI()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new RotatingAudioBands());		
+        processing.core.PApplet.runSketch( a, new MyVisual());		
 	}
 
 	public static void main(String[] args)
