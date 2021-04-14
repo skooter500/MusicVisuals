3a90bb4f0ff734c26cc4a23c3c460097418698ac
Alec Keane <ajkeane2414@gmail.com>
2021-04-14 15:53:29 +0100

    first Update

--- java/src/C19326126/Main.java
+++ java/src/C19326126/Main.java
@@ -1,5 +1,22 @@
 package C19326126;
 
-public class Main {
-    
+import example.AudioBandsVisual;
+import example.CubeVisual;
+import example.MyVisual;
+import example.RotatingAudioBands;
+
+public class Main
+{	
+
+	public void startUI()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new CubeVisual());		
+	}
+
+	public static void main(String[] args)
+	{
+		Main main = new Main();
+		main.startUI();			
+	}
 }
---
+++ java/src/C19326126/Screen1.java
@@ -0,0 +1,5 @@
+package C19326126;
+
+public class Screen1 {
+    
+}
