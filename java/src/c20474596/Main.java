25499c1e4486a49be90a8a446192fbb05b06e5b0
Gabmeister <plaza32007@gmail.com>
2022-04-12 12:32:07 +0100

    added sound and cube visual for case 2

--- java/src/c20474596/Main.java
+++ java/src/c20474596/Main.java
@@ -2,18 +2,13 @@ package c20474596;
 
 public class Main{
 
-    public static void warpdrive(){
+    public static void Start(){
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Warpdrive());
-    }
-
-    public static void test(){
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Warpdrive());
+        processing.core.PApplet.runSketch( a, new Runner());
     }
 
     public static void main(String[] args){
-        warpdrive();
+        Start();
     }
     
 }
