2a0b8068fa57ebf094639fb7093af428ba348304
habeeb <habeebalao101@gmail.com>
2022-04-14 11:47:47 +0100

    cicle

--- java/src/c20362766/HabeebsVisuals.java
+++ java/src/c20362766/HabeebsVisuals.java
@@ -1,51 +1,49 @@
 package c20362766;
 
-
 import ie.tudublin.Visual;
 import ie.tudublin.VisualException;
 
-public class HabeebsVisuals extends Visual
-{
+public class HabeebsVisuals extends Visual {
 
     WaveForm wf;
-    
-    
-    public void settings()
-    {
+    WaveForm2 wf2;
+
+    public void settings() {
         size(500, 500);
     }
 
-    public void setup()
-    {
+    public void setup() {
         startMinim();
-        
+
         // Call this instead to read audio from the microphone
-        startListening(); 
-        
+        startListening();
+
+        // new waveform instance
         wf = new WaveForm(this);
-        
+
+
+        wf2 = new WaveForm2(this);
+
     }
 
-    public void draw()
-    {
+    public void draw() {
         background(0);
-        try
-        {
+        try {
             // Call this if you want to use FFT data
-            calculateFFT(); 
-        }
-        catch(VisualException e)
-        {
+            calculateFFT();
+        } catch (VisualException e) {
             e.printStackTrace();
         }
         // Call this is you want to use frequency bands
-        calculateFrequencyBands(); 
+        calculateFrequencyBands();
 
         // Call this is you want to get the average amplitude
-        calculateAverageAmplitude();        
-        wf.render();
-        
-    }
+        calculateAverageAmplitude();
 
+
+        //wf.render();
+        wf2.render();
+
+    }
 
 }
--- java/src/c20362766/WaveForm2.java
+++ java/src/c20362766/WaveForm2.java
@@ -1,27 +1,36 @@
 package c20362766;
 
+
 import processing.core.*;
 
+
+
 // This is an example of a visual that renders the waveform
-public class WaveForm
+public class WaveForm2
 {
     HabeebsVisuals mv;
     float cy = 0;
+    float cx = 0;
 
-    public WaveForm(HabeebsVisuals mv)
+    public WaveForm2(HabeebsVisuals mv)
     {
         this.mv = mv;
         cy = this.mv.height / 2;
+        cx = this.mv.width / 2;
     }
 
     public void render()
     {
         mv.colorMode(PApplet.HSB);
+        
         for(int i = 0 ; i < mv.getAudioBuffer().size() ; i ++)
         {
+            // changes color to rainbow range
             mv.stroke(PApplet.map(i, 0, mv.getAudioBuffer().size(), 0, 255), 255, 255);
 
-            mv.line(i, cy, i, cy + cy * mv.getAudioBuffer().get(i));
+            mv.circle(cx, cy, 10000 * mv.getAudioBuffer().get(i));
+
+        
         }
     }
 }
