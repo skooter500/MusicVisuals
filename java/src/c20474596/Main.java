7e2adc218d4f55a2b7be1d32ac9455c4dd6cb968
C20447296 Sean Lawless <c20447296@mytudublin.ie>
2022-04-19 13:24:12 +0100

    bruh

--- java/src/c20474596/Main.java
+++ java/src/c20474596/Main.java
@@ -15,5 +15,6 @@ public class Main{
     public static void main(String[] args){
         warpdrive();
     }
+
+    }
     
-}
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -11,7 +11,7 @@ public class Main
 	public void startUI()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new CubeVisual());		
+        processing.core.PApplet.runSketch( a, new RotatingAudioBands());		
 	}
 
 	public static void main(String[] args)
