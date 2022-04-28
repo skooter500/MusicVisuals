04a6f3aa03bdc3a275e8b62b5ddec2c11e65ed1d
habeeb <habeebalao101@gmail.com>
2022-04-28 08:23:02 +0100

    dynamic amplitude shapes

--- java/src/c20362766/WaveForm.java
+++ java/src/c20362766/WaveForm.java
@@ -18,6 +18,7 @@ public class WaveForm {
         cy = this.mv.height / 2;
         cx = this.mv.width / 2;
 
+        System.out.println(mv.getAmplitude());
         // border squares
         for (int i = 0; i < this.mv.width; i++) {
 
@@ -25,9 +26,19 @@ public class WaveForm {
             mv.fill(118, 100);
 
             if (i % 60 == 0) {
-                mv.rect(i, 0, 35, 100);
+                mv.rect(i, 0, 35 + 35 * (mv.getSmoothedAmplitude() * 700),
+                        100 + 100 * (mv.getSmoothedAmplitude() * 700));
 
-                mv.rect(i, this.mv.height, 35, -100);
+                mv.rect(i, this.mv.height, 35 + 35 * (mv.getSmoothedAmplitude() * 700),
+                        -100 + -100 * (mv.getSmoothedAmplitude() * 700));
+
+            }
+            if (i % 35 == 0) {
+                mv.rect(i, 0, 35 + 35 * (mv.getSmoothedAmplitude() * 700),
+                        100 + 50 * (mv.getSmoothedAmplitude() * 800));
+
+                mv.rect(i, this.mv.height, 35 + 35 * (mv.getSmoothedAmplitude() * 700),
+                        -100 + -40 * (mv.getSmoothedAmplitude() * 800));
             }
         }
 
@@ -39,36 +50,40 @@ public class WaveForm {
 
             if (i % 60 == 0 || i == 0) {
 
-                mv.rect(0, i, 100, 35);
-                mv.rect(this.mv.width - 100, i + 35, this.mv.width, -35);
+                mv.rect(0, i, 100 + 100 * (mv.getSmoothedAmplitude() * 700),
+                        35 + 35 * (mv.getSmoothedAmplitude() * 700));
+                mv.rect((this.mv.width - 100) + -100 * (mv.getSmoothedAmplitude() * 700),
+                        (i + 35) + 35 * (mv.getSmoothedAmplitude() * 700), this.mv.width, -35);
 
                 // mv.circle(this.mv.width - 100, i + 35 , 35);
                 // mv.circle(this.mv.width, i, 35);
 
             }
+
+            if (i % 35 == 0 || i == 0) {
+
+                mv.rect(0, i, 100 + 50 * (mv.getSmoothedAmplitude() * 700),
+                        35 + 35 * (mv.getSmoothedAmplitude() * 700));
+                mv.rect((this.mv.width - 100) + -45 * (mv.getSmoothedAmplitude() * 700),
+                        (i + 35) + 20 * (mv.getSmoothedAmplitude() * 700), this.mv.width, -35);
+
+            }
+
         }
 
-
-       
         // eye
-        mv.ellipse(cx, cy, 400, 175);
-        mv.circle(cx, cy, 175);
-        mv.circle(cx, cy, 12);
+        mv.ellipse(cx, cy, 400 + 400 * (mv.getSmoothedAmplitude() * 700),
+                175 + 175 * (mv.getSmoothedAmplitude() * 700));
+        mv.circle(cx, cy, 175 + 175 * (mv.getSmoothedAmplitude() * 700));
+        mv.circle(cx, cy, 12 + 12 * (mv.getSmoothedAmplitude() * 700));
 
+        // upper eyelid
+        mv.line(cx + 20, cy - 90, cx + 40, cy - 190);
+        mv.line(cx - 20, cy - 90, cx - 40, cy - 190);
 
-        //upper eyelid
-        mv.line(cx +20, cy-90,cx +40, cy - 190);
-        mv.line(cx -20, cy-90,cx -40, cy - 190);
-
-
-        //bottom eyelid
-        mv.line(cx + 20, cy+90,cx +40, cy + 190);
-        mv.line(cx -20, cy+90,cx -40, cy + 190);
-        
-
-
-
-
+        // bottom eyelid
+        mv.line(cx + 20, cy + 90, cx + 40, cy + 190);
+        mv.line(cx - 20, cy + 90, cx - 40, cy + 190);
 
     }
 
