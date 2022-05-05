b4b96a3f714e2159529e9421eb09b9620f3d4567
mendebto <98464323+mendebto@users.noreply.github.com>
2022-05-05 23:35:35 +0100

    mendes stressed pt3

--- java/src/C20401562/MendesVisual.java
+++ java/src/C20401562/MendesVisual.java
@@ -6,7 +6,10 @@ import processing.core.*;
 public class MendesVisual extends Visual{
 
     Start s;
-    float cy = 0;
+    float average;
+    float sum;
+    float lerpedAverage = 0;
+    
 
     public MendesVisual(Start start) 
     {
@@ -16,13 +19,22 @@ public class MendesVisual extends Visual{
 
     public void render()
     {
+        average = 0;
+        //calculate average buffer
+        for (int i = 0; i < s.getAudioBuffer().size(); i ++)
+        {
+            sum += abs(s.getAudioBuffer().get(i));
+        }
+        average = sum / s.getAudioBuffer().size(); 
+
+        lerpedAverage = lerp(lerpedAverage, average, 0.1f); // move lerped average closer to frame 10%
         for(int i = 0; i < s.width ;i++){
             float index = PApplet.map(i, 0 , s.width, 0, s.getAudioBuffer().size());
             float y = s.getAudioBuffer().get((int)index) * 50 + s.height / 2;
 
             s.stroke(PApplet.map(index, 0, s.getAudioBuffer().size()* 2, 0, 255), 255, 255);
 
-            s.line(i, y, i, y + y* s.getAudioBuffer().get((int)index));
+            s.line(i, y, i, y + s.getAudioBuffer().get((int)index));
 
         }
     }
