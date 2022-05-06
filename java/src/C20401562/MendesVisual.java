cc32b189c9fd4aaf77d497b00c6bb25b75e25eda
mendebto <98464323+mendebto@users.noreply.github.com>
2022-05-06 12:28:10 +0100

    mendes final final commit

--- java/src/C20401562/MendesVisual.java
+++ java/src/C20401562/MendesVisual.java
@@ -6,58 +6,64 @@ import java.util.*;
 
 public class MendesVisual extends Visual{
 
-    Start start;
+    Start s;
     float lerpedAverage = 0;
     float y= 0;
-    float smallpend = 0;
-    float smallpstart = 15;
-    float bigpstart = 0;
+    float bigpend = 0;
+    float bigpstart = 15;
+    float smallpstart = 0;
+    float space = 0.2f;
     
     public MendesVisual(Start start) 
     {
-        this.start = start;
+        this.s = start;
     }
 
     public void render()
     {
         
-        
-        start.pushMatrix();
-        start.getFFT();
-        start.translate(start.width/2, start.height/2);
-        start.fill(170,255,250);
-        start.circle(0, 0, 600);
-        start.strokeWeight(start.width/120);
-        start.text("Game Over", -80, 250);
-        if (start.beat.isOnset() == true) {
-            smallpstart =smallpstart + 5 % 255;
-            smallpend =smallpend + random(200) % 255;                    
+
+        s.pushMatrix();
+        s.fill(map(s.getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
+        s.noStroke();
+        s.translate(s.width/2, s.height/2);
+        s.fill(170,255,250);
+        s.circle(0, 0, 600);
+        s.strokeWeight(s.width/120);
+       
+        if (s.beat.isOnset() == true) {
+            bigpstart =bigpstart + 5 % 255;
+            bigpend =bigpend + random(200) % 255;                    
         }
 
-        if (start.beat.isHat()) {
-            bigpstart = bigpstart + 120;
+        if (s.beat.isHat()) {
+            smallpstart = smallpstart + 120;
         }
         
         for(float i = 0f; i < 360; i++){
             //big pupil size
-            float pb = PApplet.map(y, 0, 1, (start.getSmoothedAmplitude()*500) * -1 ,start.getSmoothedAmplitude()*300);
+            float pb = PApplet.map(y, 0, 1, (s.getSmoothedAmplitude()*600) * -1 ,s.getSmoothedAmplitude()*300);
 
             //colour to change with the beat
-            start.stroke(
-                PApplet.map(i, 0, start.getAudioBuffer().size(),smallpstart , smallpend), 255, 255
+            s.stroke(
+                PApplet.map(i, 0, s.getAudioBuffer().size(),bigpstart , bigpend), 255, 255
                 );
-            start.ellipse(300,0,pb,10);
-        }
+            s.ellipse(290,0,pb,10);
+
+            s.rotate(space);
+        } 
 
         for(float i = 0f; i < 360; i++){
             //small pupil size
-            float ps = PApplet.map(y, 0, 1, (start.getSmoothedAmplitude() * 1000) * 1 ,start.getSmoothedAmplitude()*30);
+            float ps = PApplet.map(y, 0, 1, (s.getSmoothedAmplitude() * 1000) * 1 ,s.getSmoothedAmplitude()*50);
             //colour to change with the beat
-            start.stroke(
-                PApplet.map(i, 0, start.getAudioBuffer().size(),255 , 255), 255, bigpstart);
-            start.ellipse(5,0,0,ps);
+            s.stroke(
+                PApplet.map(i, 0, s.getAudioBuffer().size(),255 , smallpstart), 255, smallpstart);
+            s.ellipse(5,0,0,ps);
+
+            s.rotate(space);
             }
-        start.popMatrix();
+        s.popMatrix();
     }
     
 }
