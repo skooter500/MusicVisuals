867209cd455c45df6298ec716313e605acf1fd5c
Stephanie <stefniogu@gmail.com>
2023-03-22 18:29:05 +0000

    br1 Creating a deviding of music visualisation functionality

--- java/src/example/MyVisual.java
+++ java/src/example/MyVisual.java
@@ -1,5 +1,6 @@
 package example;
 
+
 import ie.tudublin.*;
 
 public class MyVisual extends Visual
@@ -7,14 +8,18 @@ public class MyVisual extends Visual
     WaveForm wf;
     AudioBandsVisual abv;
 
+    int mode = 1;
+    int numbersOfPurts = 3;
+    boolean lastPressed = false;
+
     public void settings()
     {
-        size(1024, 500);
+        size(1024, 1024);
         
         // Use this to make fullscreen
-        //fullScreen();
+        // fullScreen();
 
-        // Use this to make fullscreen and use P3D for 3D graphics
+        ///Use this to make fullscreen and use P3D for 3D graphics
         //fullScreen(P3D, SPAN); 
     }
 
@@ -42,24 +47,65 @@ public class MyVisual extends Visual
         }
     }
 
-    public void draw()
-    {
+    @Override
+    public void draw(){
         background(0);
-        try
-        {
-            // Call this if you want to use FFT data
-            calculateFFT(); 
-        }
-        catch(VisualException e)
-        {
-            e.printStackTrace();
-        }
-        // Call this is you want to use frequency bands
-        calculateFrequencyBands(); 
 
-        // Call this is you want to get the average amplitude
-        calculateAverageAmplitude();        
-        wf.render();
-        abv.render();
+		switch(mode) {
+            case 1: 
+                partOne();
+                break;
+            case 2: 
+                partTwo(); 
+                break;
+            case 0: 
+                partThree();
+                break;
+            default:
+                break;
+        }   
+
+        keyPressingLogic();
+}
+
+    void keyPressingLogic(){
+        if(keyPressed){
+            if(!lastPressed){
+                mode = (mode+1) % numbersOfPurts;
+            }
+            lastPressed = true;
+        } else{
+            lastPressed = false;
+        }
+    }
+
+    void partOne(){
+        try
+            {
+                // Call this if you want to use FFT data
+                calculateFFT(); 
+            }
+            catch(VisualException e)
+            {
+                e.printStackTrace();
+            }
+            // Call this is you want to use frequency bands
+            calculateFrequencyBands(); 
+
+            // Call this is you want to get the average amplitude
+            calculateAverageAmplitude();        
+            wf.render();
+            abv.render();
+    }
+
+    void partTwo(){
+        color(255);
+        text("Part Two", 100, 100);
+
+    }
+
+    void partThree(){
+        color(255);
+        text("Part Three", 100, 100);
     }
 }
