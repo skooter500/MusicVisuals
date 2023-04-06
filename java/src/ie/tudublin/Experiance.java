287cdeb14c458617e1a0254cdcd9f076beced533
Hadassah <105879604+MAYRIAM@users.noreply.github.com>
2023-04-06 14:41:31 +0100

    update the Experiance file

--- java/src/ie/tudublin/Experiance.java
+++ java/src/ie/tudublin/Experiance.java
@@ -1,11 +1,87 @@
 package ie.tudublin;
 
-public class Experiance {
+import ddf.minim.AudioBuffer;
+import ddf.minim.AudioInput;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+import ddf.minim.analysis.FFT;
+import processing.core.PApplet;
+
+public class Experiance extends PApplet{
+
+    Minim minim;
+    AudioPlayer ap;
+    AudioInput ai;
+    AudioBuffer ab;
+
+    FFT fft;
+
+    int mode = 0;
+
+    float y = 0;
     
-    void setup() {
-        
-        size(800,600);
-        background(0);
+
+    public void keyPressed() {
+		if (key >= '0' && key <= '9') {
+			mode = key - '0';
+		}
+		if (keyCode == ' ') {
+            if (ap.isPlaying()) {
+                ap.pause();
+            } else {
+                ap.rewind();
+                ap.play();
+            }
+        }
+	}
+    
+    public void settings()
+    {
+        size(1024, 1024);
+    }
+
+    public void setup()
+    {
+        minim = new Minim(this);
+        // Uncomment this to use the microphone
+        // ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
+        // ab = ai.mix; 
+
+        // And comment the next two lines out
+        ap = minim.loadFile("MusicVisuals/java/data/Victoria_Mon_t_ft_Khalid_-_Experience.mp3", 1024);
+        ap.play();
+        ab = ap.mix;
+        colorMode(HSB);
+    }
+
+    public void draw()
+    {
+        switch (mode) {
+			case 0://first to play (grace)
+
+          
+          
+                
+                break;
+        case 1://When you press key 1 (hadassah)
+            background(0);
+                    
+            break;
+
+        case 2:// when you press key 2 (cece)
+            
+
+            break;
+
+        case 3://when you press key 3 (molly)
+
+            break;
+
+        case 4://when you press key 4 (aisha)
+
+            break;
+        }
+
         
     }
 
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -10,7 +10,7 @@ public class Main
 	public void startUI()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new MyVisual());		
+        processing.core.PApplet.runSketch( a, new Experiance());		
 	}
 
 	public static void main(String[] args)
