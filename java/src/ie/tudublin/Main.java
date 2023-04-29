1197e8c88fa88d18752f90ec010a3b1bf5e47155
DemiAina <100835583+DemiAina@users.noreply.github.com>
2023-04-29 17:26:57 +0100

    DemiVisualiser Text

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,21 +1,19 @@
 package ie.tudublin;
 
+import C21725659.DemiAina;
 import example.CubeVisual;
 import example.MyVisual;
 import example.RotatingAudioBands;
 
-public class Main
-{	
+public class Main {
 
-	public void startUI()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new MyVisual());		
+	public void startUI() {
+		String[] a = { "MAIN" };
+		processing.core.PApplet.runSketch(a, new DemiAina());
 	}
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		Main main = new Main();
-		main.startUI();			
+		main.startUI();
 	}
 }
