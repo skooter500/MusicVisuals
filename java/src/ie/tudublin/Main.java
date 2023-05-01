8e3830973408695c2532126703b50ade7ff4af51
Bayan-AlWardi <123604689+Bayan-AlWardi@users.noreply.github.com>
2023-05-01 18:36:52 +0100

    run 2 visuals

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,19 +1,17 @@
 package ie.tudublin;
 
-import D20125707.CDWaves;
-
 public class Main
 {
 
 	
 
-	public static void CDWaves()
+	public static void Assignment()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new CDWaves());
+        processing.core.PApplet.runSketch( a, new Assignment());
     }
 	public static void main(String[] args)
 	{
-		CDWaves();
+		Assignment();
 	}
 }
