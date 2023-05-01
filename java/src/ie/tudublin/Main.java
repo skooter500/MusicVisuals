45476c9fb96265c83a38d532724d595c340e90f0
Grace <gracerosevalentine@gmail.com>
2023-05-01 17:32:29 +0100

    backup

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -10,7 +10,7 @@ public class Main
 	public void startUI()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new MyVisual());		
+        processing.core.PApplet.runSketch( a, new RotatingAudioBands());		
 	}
 
 	public static void main(String[] args)
