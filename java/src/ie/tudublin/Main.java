7f6874ba3ca07cc5d4777d9e54ba35346a46ef54
Hadassah <105879604+MAYRIAM@users.noreply.github.com>
2023-05-02 16:24:02 +0100

    made a branch for cece and molly and have addded their repsective file to it

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -6,7 +6,7 @@ public class Main
 	public void startUI()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Experiance());		
+        processing.core.PApplet.runSketch( a, new stars());		
 	}
 
 
