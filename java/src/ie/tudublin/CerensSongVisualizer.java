016ea139b2dfa0c095b2ce7220a8618ba048293f
cucannucan <cerenucan02@gmail.com>
2023-05-03 01:36:54 +0100

    added the comments

--- java/src/ie/tudublin/CerensSongVisualizer.java
+++ java/src/ie/tudublin/CerensSongVisualizer.java
@@ -6,157 +6,164 @@ import ddf.minim.Minim;
 import processing.core.PApplet;
 
 public class CerensSongVisualizer extends PApplet {
-    Minim minim;
-    AudioPlayer ap;
-    AudioBuffer ab;
+    Minim minim; // Audio library object
+    AudioPlayer ap; // Audio player object
+    AudioBuffer ab; // Audio buffer object
 
-    private static final int NUM_PARTICLES = 5000;
-    private Particle[] particles;
+    private static final int NUM_PARTICLES = 5000; // Number of particles
+    private Particle[] particles; // Array to store particles
 
     private float[] waveform; // Array to store the waveform data
 
-    private float rotationAngle = 0;
-    private float rotationSpeed = 0.01f;
+    private float rotationAngle = 0; // Angle for rotation
+    private float rotationSpeed = 0.01f; // Speed of rotation
 
     public void settings() {
-        size(800, 600);
+        size(800, 600); // Set the size of the window
     }
 
     public void setup() {
-        minim = new Minim(this);
-        particles = new Particle[NUM_PARTICLES];
+        minim = new Minim(this); // Initialize the minim object
+        particles = new Particle[NUM_PARTICLES]; // Initialize the particle array
 
         for (int i = 0; i < NUM_PARTICLES; i++) {
-            float angle = random(TWO_PI);
-            float radius = random(width * 0.4f);
-            float speed = random(0.5f, 2.0f);
-            float size = random(1, 4);
+            float angle = random(TWO_PI); // Random angle
+            float radius = random(width * 0.4f); // Random radius within a range
+            float speed = random(0.5f, 2.0f); // Random speed within a range
+            float size = random(1, 4); // Random size within a range
 
-            particles[i] = new Particle(angle, radius, speed, size);
+            particles[i] = new Particle(angle, radius, speed, size); // Create a new particle
         }
 
-        smooth();
+        smooth(); // Enable smoothing of edges
 
-        ap = minim.loadFile("java/data/Parasite.mp3", 1024);
-        ap.play();
-        ab = ap.mix;
+        ap = minim.loadFile("java/data/Parasite.mp3", 1024); // Load the audio file
+        ap.play(); // Start playing the audio
+        ab = ap.mix; // Get the audio buffer
 
-        colorMode(HSB, 360, 100, 100);
+        colorMode(HSB, 360, 100, 100); // Set the color mode
 
         waveform = ab.toArray(); // Convert audio buffer to an array
     }
-
 
     public void draw() {
         background(0); // Clear the background
 
         // Update and display particles
         for (Particle particle : particles) {
-            particle.update();
-            particle.display();
+            particle.update(); // Update the particle
+            particle.display(); // Display the particle
         }
 
-        drawWaveform();
-        rotateStar();
+        drawWaveform(); // Draw the waveform
+        rotateStar(); // Rotate the star
     }
 
     private void drawWaveform() {
-        float waveformHeight = height / 2;
-        noFill();
-        strokeWeight(1);
+        float waveformHeight = height / 2; // Height of the waveform
+        noFill(); // Don't fill shapes
+        strokeWeight(1); // Set stroke weight
 
         // Draw waveform graph
         beginShape();
         for (int i = 0; i < waveform.length; i += 10) {
-            float waveX = map(i, 0, waveform.length - 1, 0, width);
-            float waveY = map(waveform[i], -1, 1, waveformHeight, -waveformHeight);
-            stroke(i % 360, 100, 100);
-            vertex(waveX, waveY);
+            float waveX = map(i, 0, waveform.length - 1, 0, width); // Map x-coordinate
+            float waveY = map(waveform[i], -1, 1, waveformHeight, -waveformHeight); // Map y-coordinate based on
+                                                                                    // waveform data
+            stroke(i % 360, 100, 100); // Set stroke color based on index
+            vertex(waveX, waveY); // Add vertex to the shape
         }
         endShape();
     }
 
     private void rotateStar() {
-        translate(width / 2, height / 2);
-        rotate(rotationAngle);
+        translate(width / 2, height / 2); // Translate to the center of the window
+        rotate(rotationAngle); // Apply rotation transformation
 
         float amplitude = max(abs(ap.left.get(0)), abs(ap.right.get(0))); // Get the maximum amplitude
         float starSize = map(amplitude, 0, 1, 50, 200); // Map amplitude to star size
 
-        noStroke();
-        int npoints = floor(map(sin(starSize * 0.3f), -1, 1, 1, 12));
-        drawStar(0, 0, starSize, starSize / 2, npoints); // Draw a 5-pointed star
+        noStroke(); // Disable stroke
+        int npoints = floor(map(sin(starSize * 0.3f), -1, 1, 1, 12)); // Calculate the number of points for the star
+                                                                      // shape
+        drawStar(0, 0, starSize, starSize / 2, npoints); // Draw a star shape with variable number of points
         drawStar(0, 0, starSize, starSize / 2, 5); // Draw a 5-pointed star
-        rotationAngle += rotationSpeed;
+        rotationAngle += rotationSpeed; // Update the rotation angle
     }
 
     private void drawStar(float x, float y, float radius1, float radius2, int npoints) {
         float amplitude = max(abs(ap.left.get(0)), abs(ap.right.get(0))); // Get the maximum amplitude
         float hueValue = map(amplitude, 0, 1, 0, 360); // Map amplitude to hue value
 
-        fill(hueValue, 100, 100);
-        drawStarShape(x, y, radius1, radius2, npoints);
+        fill(hueValue, 100, 100); // Set fill color based on hue value
+        drawStarShape(x, y, radius1, radius2, npoints); // Draw the star shape
     }
 
-        // Helper method to draw a star shape
-        private void drawStarShape(float x, float y, float radius1, float radius2, int npoints) {
-            float angle = TWO_PI / npoints;
-            float halfAngle = angle / 2.0f;
-            beginShape();
-            for (float a = 0; a < TWO_PI; a += angle) {
-                float sx = x + cos(a) * radius2;
-                float sy = y + sin(a) * radius2;
-                vertex(sx, sy);
-                sx = x + cos(a + halfAngle) * radius1;
-                sy = y + sin(a + halfAngle) * radius1;
-                vertex(sx, sy);
-            }
-            endShape(CLOSE);
+    // Helper method to draw a star shape
+    private void drawStarShape(float x, float y, float radius1, float radius2, int npoints) {
+        float angle = TWO_PI / npoints; // Calculate the angle between each point
+        float halfAngle = angle / 2.0f; // Calculate the half angle
+
+        beginShape();
+        for (float a = 0; a < TWO_PI; a += angle) {
+            float sx = x + cos(a) * radius2; // Calculate x-coordinate for outer radius
+            float sy = y + sin(a) * radius2; // Calculate y-coordinate for outer radius
+            vertex(sx, sy); // Add vertex to the shape
+            sx = x + cos(a + halfAngle) * radius1; // Calculate x-coordinate for inner radius
+            sy = y + sin(a + halfAngle) * radius1; // Calculate y-coordinate for inner radius
+            vertex(sx, sy); // Add vertex to the shape
         }
-    
-        private class Particle {
-            private float angle;
-            private float radius;
-            private float speed;
-            private float size;
-            private float rotation;
-    
-            Particle(float angle, float radius, float speed, float size) {
-                this.angle = angle;
-                this.radius = radius;
-                this.speed = random(2.0f, 1.0f); // Adjust the speed range here
-                this.size = size;
-                this.rotation = 0;
-            }
-    
-            void update() {
-                radius += speed;
-                if (radius > 400) {
-                    radius = 0;
-                }
-            }
-    
-            void display() {
-                float x = width / 2 + cos(angle) * radius;
-                float y = height / 2 + sin(angle) * radius;
-    
-                float amplitude = ab.get((int) (angle * 10)) * 100; // Get amplitude from audio buffer
-    
-                float hueValue = (angle + amplitude) % 360; // Vary the hue based on angle and amplitude
-                float sizeValue = size + amplitude * 0.05f; // Vary the size based on amplitude
-    
-                pushMatrix();
-                translate(x, y);
-                rotate(rotation);
-                noStroke();
-                fill(hueValue, 100, 100);
-                ellipse(0, 0, sizeValue, sizeValue);
-                popMatrix();
+        endShape(CLOSE); // Close the shape
+    }
+
+    private class Particle {
+        private float angle; // Angle of the particle
+        private float radius; // Distance from the center of the screen
+        private float speed; // Speed at which the particle moves
+        private float size; // Size of the particle
+        private float rotation; // Rotation angle of the particle
+
+        Particle(float angle, float radius, float speed, float size) {
+            this.angle = angle;
+            this.radius = radius;
+            this.speed = random(2.0f, 1.0f); // Adjust the speed range here
+            this.size = size;
+            this.rotation = 0;
+        }
+
+        void update() {
+            radius += speed; // Update the radius based on the speed
+            if (radius > 400) {
+                radius = 0; // Reset the radius if it exceeds a certain value
             }
         }
-    
-        public static void main(String[] args) {
-            PApplet.main("ie.tudublin.CerensSongVisualizer");
+
+        void display() {
+            float x = width / 2 + cos(angle) * radius; // Calculate the x-coordinate of the particle
+            float y = height / 2 + sin(angle) * radius; // Calculate the y-coordinate of the particle
+
+            // Get the amplitude from the audio buffer based on the angle
+            float amplitude = ab.get((int) (angle * 10)) * 100;
+
+            // Vary the hue based on the sum of the angle and amplitude
+            float hueValue = (angle + amplitude) % 360;
+
+            // Vary the size based on the amplitude
+            float sizeValue = size + amplitude * 0.05f;
+
+            pushMatrix(); // Save the current transformation matrix
+            translate(x, y); // Translate to the particle's position
+            rotate(rotation); // Apply rotation transformation
+
+            noStroke(); // Disable stroke
+            fill(hueValue, 100, 100); // Set fill color based on hue value
+            ellipse(0, 0, sizeValue, sizeValue); // Draw the particle as an ellipse
+
+            popMatrix(); // Restore the previous transformation matrix
         }
     }
-    
+
+    public static void main(String[] args) {
+        PApplet.main("ie.tudublin.CerensSongVisualizer"); // Launch the application
+    }
+}
--- java/src/ie/tudublin/MusicVisualizer.java
+++ java/src/ie/tudublin/MusicVisualizer.java
@@ -1,47 +1,47 @@
 package ie.tudublin;
 
-import ddf.minim.*;
+import ddf.minim.*; // Import Minim library for audio processing
 import ddf.minim.analysis.*;
 import processing.core.PApplet;
 import java.util.ArrayList;
 import processing.core.PVector;
 
 public class MusicVisualizer extends PApplet {
-  Minim minim;
-  AudioPlayer player;
-  BeatDetect beat;
-  FFT fft;
+  Minim minim; // Minim object for audio processing
+  AudioPlayer player; // AudioPlayer object for playing audio
+  BeatDetect beat; // BeatDetect object for beat detection
+  FFT fft; // FFT object for frequency analysis
 
-  float angle = 0;
-  float size = 50;
-  float posX, posY;
-  float speedX, speedY;
+  float angle = 0; // Angle for rotation
+  float size = 50; // Size of design element
+  float posX, posY; // Position of design element
+  float speedX, speedY; // Speed of design element
 
-  ArrayList<Particle> particles;
+  ArrayList<Particle> particles; // List to store particles
 
   public void settings() {
-    size(800, 600);
+    size(800, 600); // Set the size of the window
   }
 
   public void setup() {
-    minim = new Minim(this);
+    minim = new Minim(this); // Initialize the Minim object
 
-    posX = width / 2;
-    posY = height / 2;
-    speedX = random(-2, 2);
-    speedY = random(-2, 2);
+    posX = width / 2; // Set initial X position of design element
+    posY = height / 2; // Set initial Y position of design element
+    speedX = random(-2, 2); // Set initial X speed of design element
+    speedY = random(-2, 2); // Set initial Y speed of design element
 
-    player = minim.loadFile("java/data/Parasite.mp3");
-    player.play();
+    player = minim.loadFile("java/data/Parasite.mp3"); // Load the audio file
+    player.play(); // Start playing the audio
 
-    beat = new BeatDetect();
-    fft = new FFT(player.bufferSize(), player.sampleRate());
+    beat = new BeatDetect(); // Initialize the BeatDetect object
+    fft = new FFT(player.bufferSize(), player.sampleRate()); // Initialize the FFT object
 
-    particles = new ArrayList<Particle>();
+    particles = new ArrayList<Particle>(); // Initialize the list for particles
   }
 
   public void draw() {
-    background(0);
+    background(0); // Set the background color
 
     // Analyze the audio for beats
     beat.detect(player.mix);
@@ -93,26 +93,26 @@ public class MusicVisualizer extends PApplet {
 
   void changeVisualEffect() {
     // Modify the design element based on the beat
-    size = random(10, 100);
-    speedX = random(-5, 5);
-    speedY = random(-5, 5);
-    background(random(255), random(255), random(255));
+    size = random(10, 100); // Randomize the size of the design element
+    speedX = random(-5, 5); // Randomize the horizontal speed of the design element
+    speedY = random(-5, 5); // Randomize the vertical speed of the design element
+    background(random(255), random(255), random(255)); // Randomize the background color
   }
-
+  
   void drawVisualEffect() {
     // Draw the design element in all four corners
     for (int i = 0; i < 4; i++) {
       float cornerX = i % 2 == 0 ? posX : width - posX;
       float cornerY = i < 2 ? posY : height - posY;
-
+  
       pushMatrix();
       translate(cornerX, cornerY);
       rotate(angle);
       rectMode(CENTER);
-      fill(random(255), random(255), random(255));
+      fill(random(255), random(255), random(255)); // Randomize the fill color of the design element
       rect(0, 0, size, size);
       popMatrix();
-
+  
       // Create particles based on the beat
       if (beat.isOnset()) {
         for (int j = 0; j < 10; j++) {
@@ -121,15 +121,15 @@ public class MusicVisualizer extends PApplet {
         }
       }
     }
-
+  
     // Rotate the design element gradually
     angle += 0.05;
   }
-
+  
   public static void main(String[] args) {
     PApplet.main("MusicVisualizer");
   }
-
+  
   class Particle {
     PVector position;
     PVector velocity;
@@ -137,37 +137,38 @@ public class MusicVisualizer extends PApplet {
     float lifespan;
     float size;
     float hue;
-
+  
     Particle(float x, float y, float speedX, float speedY) {
       position = new PVector(x, y);
-      velocity = new PVector(speedX, speedY).add(PVector.random2D().mult(random(1, 5)));
-      acceleration = new PVector(0, 0.05f);
+      velocity = new PVector(speedX, speedY).add(PVector.random2D().mult(random(1, 5))); // Randomize the initial velocity of particles
+      acceleration = new PVector(0, 0.05f); // Set the constant acceleration for particles
       lifespan = 255;
-      size = random(4, 16);
-      hue = random(360);
+      size = random(4, 16); // Randomize the size of particles
+      hue = random(360); // Randomize the hue value of particles
     }
-
+  
     void update() {
-      velocity.add(acceleration);
-      position.add(velocity);
-      lifespan -= 2;
+      velocity.add(acceleration); // Update the velocity of particles
+      position.add(velocity); // Update the position of particles based on velocity
+      lifespan -= 2; // Decrease the lifespan of particles
     }
-
+  
     void display() {
       colorMode(HSB);
       noStroke();
-      fill(hue, 255, 255, lifespan);
-      ellipse(position.x, position.y, size, size);
+      fill(hue, 255, 255, lifespan); // Set the fill color of particles with transparency based on lifespan
+      ellipse(position.x, position.y, size, size); // Draw particles as ellipses
     }
-
+  
     boolean isDead() {
-      return lifespan <= 0;
+      return lifespan <= 0; // Check if the lifespan of particles has reached zero
     }
   }
-
+  
   public void stop() {
     player.close();
     minim.stop();
     super.stop();
   }
 }
+
