c02b2b87710393c3ed0ff5d9e7ac574dc85b6131
Entroshock <alextsang@live.ie>
2023-05-03 16:15:08 +0100

    my images?

--- java/src/ie/tudublin/MusicVisualizer.java
+++ java/src/ie/tudublin/MusicVisualizer.java
@@ -90,9 +90,10 @@ public class MusicVisualizer extends PApplet {
         this.visual4 = new Visual4(this, this.audioBuffer, this.audioPlayer, this.windowWidth, this.windowHeight);
 
 
+        visualList.add(startMenuVisual);
+        visualList.add(visual3);
+        visualList.add(visual2);
         visualList.add(visual4);
-        visualList.add(visual1);
-        visualList.add(visual1);
         visualList.add(visual1);
         visualList.add(visual1);
         visualList.add(visual1);
