085134223d82e3948ab9639b1e53ce2c817e5e14
AlexMackey1 <C22739165@mytudublin.ie>
2024-03-14 11:34:54 +0000

    Added started code to my visual

--- java/src/C22739165/Alex.java
+++ java/src/C22739165/Alex.java
@@ -1,4 +1,45 @@
 package C22739165;
-public class Alex {
+
+import example.AudioBandsVisual;
+import example.WaveForm;
+import ie.tudublin.*;
+
+
+public class Alex extends Visual{
     
+
+    public void settings() {
+        size(1024, 500);
+
+        // Use this to make fullscreen
+        // fullScreen();
+
+        // Use this to make fullscreen and use P3D for 3D graphics
+        // fullScreen(P3D, SPAN);
+    }
+
+    public void setup() {
+        startMinim();
+
+        // Call loadAudio to load an audio file to process
+        // loadAudio("heroplanet.mp3");
+
+        // Call this instead to read audio from the microphone
+        startListening();
+
+        
+    }
+
+    public void keyPressed() {
+        if (key == ' ') {
+            getAudioPlayer().cue(0);
+            getAudioPlayer().play();
+        }
+    }
+
+    public void draw() {
+        background(0);
+        circle(width/2, height/2, 200);
+    }
 }
+
