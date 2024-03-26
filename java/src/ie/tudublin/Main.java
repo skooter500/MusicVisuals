0ddacf05716712ca412b9d472e4e4173adbf7c98
Aleena <C22790201@mytudublin.ie>
2024-03-26 13:18:04 +0000

    deleted branch

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     public void startUI() {
         String[] a = { "MAIN" };
-        processing.core.PApplet.runSketch(a, new MyVisual());
+        processing.core.PApplet.runSketch(a, new AleenaVisual1());
     }
 
     public static void main(String[] args) {
